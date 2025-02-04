b6b127efc29bb836f95d40825cf26e2a26cb1fa6
Subroto Biswas <subroto.iu@gmail.com>
2025-02-04 21:18:57 +0600

    Update Doc Readme

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-let map = new Map();
-
-map.set("name", "John");
-
-let keys = Array.from(map.keys());
-
-// Error: keys.push is not a function
-keys.push("more");
-
-console.log(keys);
+const numbers = [2, 3, 1];
+numbers.sort();
+console.log(numbers);
